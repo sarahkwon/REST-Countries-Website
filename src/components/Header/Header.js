ecb6a426cd0598d915cd57aa0689416067b036b1
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-03-19 04:40:23 -0700

    Style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,10 @@ import '../../styles/Header/Header.css'
 const Header = () => {
   return (
     <header>
-      <h1>Where in the world?</h1>
-      <DarkModeToggleButton/>
+      <div className='header-contents'>
+        <h1>Where in the world?</h1>
+        <DarkModeToggleButton/>
+      </div>
     </header>
   );
 }
