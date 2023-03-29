7e1f0b1a1239eb8945bffc41138dec3b6b6bf87b
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-03-29 01:35:15 -0700

    Change background color and update max-width of app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header.js'
+import SearchFilter from './components/SearchFilter/SearchFilter'
 
 function App() {
   return (
     <main>
       <Header/>
       <div className='App'>
-
+        <SearchFilter/>
       </div>
     </main>
   );
