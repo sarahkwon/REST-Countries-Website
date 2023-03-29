a9afbfb832bc28a28a20f81a87f3e45730e13f03
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-03-29 00:34:53 -0700

    Make header responsive for mobile devices

--- src/components/Header/DarkModeToggleButton.js
+++ src/components/Header/DarkModeToggleButton.js
@@ -1,12 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMoon } from '@fortawesome/free-solid-svg-icons'
+// import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faMoon } from '@fortawesome/free-regular-svg-icons'
 
 import '../../styles/Header/DarkModeToggleButton.css'
 
 const DarkModeToggleButton = () => {
   return (
     <button className='dark-mode-toggle-button'>
-      <FontAwesomeIcon icon={faMoon} className='fa fa-lg'/>
+      <FontAwesomeIcon icon={faMoon} className='fa'/>
       <p>Dark Mode</p>
     </button>
   )
