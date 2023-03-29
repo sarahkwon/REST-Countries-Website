562b2d0aab098b2cbfe3351459ad5721ca1e074b
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-03-28 21:35:39 -0700

    Create filter dropdown component

---
+++ src/components/SearchFilter/FilterDropDown.js
@@ -0,0 +1,23 @@
+
+
+import '../../styles/SearchFilter/FilterDropDown.css'
+
+const FilterDropDown = () => {
+  return (
+    <div className='filterdropdown_container'>
+      <select id='region' name='region'>
+        <option selected disabled hidden>Filter by Region</option>
+        <option>Some</option>
+        <option>Arbitrary</option>
+        <option>Options</option>
+        <option>That</option>
+        <option>Will</option>
+        <option>Be</option>
+        <option>Mapped</option>
+        <option>Out</option>
+      </select>
+    </div>
+  )
+}
+
+export default FilterDropDown
