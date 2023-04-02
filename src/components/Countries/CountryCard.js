b33c996a2676f55bbbbe8eeb0f7d279127c74915
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-04-01 19:00:56 -0700

    Style country card

--- src/components/Countries/CountryCard.js
+++ src/components/Countries/CountryCard.js
@@ -5,21 +5,23 @@ const CountryCard = () => {
   return (
     <article className='country-card'>
       <img src="https://flagcdn.com/us.svg" alt='Flag of America'/>
-      <h2>Country Name</h2>
-      <ul>
-        <li>
-          <b>Population: </b>
-          999,999,999
-        </li>
-        <li>
-          <b>Region: </b>
-          Americas
-        </li>
-        <li>
-          <b>Capital: </b>
-          Washington D.C.
-        </li>
-      </ul>
+      <div className='desc-container'>
+        <h2>Country Name</h2>
+        <ul className='desc'>
+          <li>
+            <b>Population: </b>
+            999,999,999
+          </li>
+          <li>
+            <b>Region: </b>
+            Americas
+          </li>
+          <li>
+            <b>Capital: </b>
+            Washington D.C.
+          </li>
+        </ul>
+      </div>
     </article>
   );
 }
